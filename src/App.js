5a04379f72ef7e26f9535f7919fcc1a201d1a8b4
rob4abcba <rob_abcba@yahoo.com>
2019-06-24 16:55:33 -0700

    Display Todos component from App.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos />
     </div>
   );
 }
