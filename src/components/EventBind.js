a5de910f45fc6e3c2174b93d369269106f300a9a
rob4abcba <rob_abcba@yahoo.com>
2019-06-29 01:51:18 -0700

    Approach1: bind(this) in the render method

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -3,16 +3,28 @@ import React, { Component } from 'react'
 class EventBind extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             message: "EventBind.js message"
+            message: "EventBind.js message"
         }
     }
-    
+
+    clickHandler() {
+        this.setState({
+            message: "You clicked the EventBind.js button!"
+        })
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
-                {this.state.message}
+                <div>
+                    {this.state.message}
+                </div>
+                <button onClick={this.clickHandler.bind(this)}>
+                    Click this button to change message.
+                </button>
             </div>
         )
     }
