8af2d6067ea62b221001c438001338b71361145e
rob4abcba <rob_abcba@yahoo.com>
2019-06-28 21:36:14 -0700

    Add this.props to Class component Welcome.

--- src/App.js
+++ src/App.js
@@ -9,43 +9,46 @@ import Hello from './components/Hello';
 
 class App extends React.Component {
   constructor(props) {
-  super(props);
-  this.state = {
-    todos: [
-      {
-        id: 1,
-        title: 'Take out the garbage',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Eat dinner',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Do taxes',
-        completed: false
-      }
-    ]
-  };
-}
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Take out the garbage',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Eat dinner',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Do taxes',
+          completed: false
+        }
+      ]
+    };
+  }
   render() {
-  return (
-    <div className="App">
-      <MyGreet name="Clark" heroName="Superman">
-        <p>Optional p section</p>
-      </MyGreet>
-      <MyGreet name="Bruce" heroName="Batman">
-        <button>Awesome button</button>
-      </MyGreet>
-      <Welcome />
-      <Hello />
-      <Counter />
-      <Message />
-      <Todos />
-    </div>
-  );
+    return (
+      <div className="App">
+        <MyGreet name="Clark" heroName="Superman">
+          <p>Optional p section</p>
+        </MyGreet>
+        <MyGreet name="Bruce" heroName="Batman">
+          <button>Awesome button</button>
+        </MyGreet>
+        <Welcome name="Clark" heroName="Superman">
+          <p>Optional p section</p>
+        </Welcome>
+        <Welcome name="Bruce" heroName="Batman" />
+        <Hello />
+        <Counter />
+        <Message />
+        <Todos />
+      </div>
+    );
   }
 }
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react'
 
 class Welcome extends Component {
     render() {
-        return <h1>Class Component</h1>
+        return (
+            <div>
+                <h1>Welcome {this.props.name} aka {this.props.heroName}</h1>
+                {this.props.children}
+            </div>
+        )
     }
 }
 
