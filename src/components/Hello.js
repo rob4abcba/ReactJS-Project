ed7666c949a61d36aae195b50d1225c2c455998e
rob4abcba <rob_abcba@yahoo.com>
2019-06-28 20:20:03 -0700

    Use React.createElement instead of JSX.

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -5,11 +5,16 @@ import React from 'react'
 // }
 
 const Hello = () => {
-return (
-<div>
-    <h1>Hello Functional Component with return div</h1>
-</div>
-)
+    // return (
+    //     <div>
+    //         <h1>Hello Functional Component with return div</h1>
+    //     </div>
+    // )
+    return React.createElement(
+        'div',
+        {id: 'hello', className: 'dummyClass'},
+        React.createElement('h1', null, 'Hello Complicated no JSX createElement method')
+    )
 }
 
 export default Hello
