ccbd5a46667f9001ecb8671cbc961fd428be008c
rob4abcba <rob_abcba@yahoo.com>
2019-06-30 22:24:09 -0700

    Conditional Rendering Method3 Tertiary Operator

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -10,19 +10,37 @@ class UserGreeting extends Component {
     }
 
     render() {
-        if (this.state.isLoggedIn) {
-            return (
-                <div>
-                    <div>Welcome SvMathTutor</div>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <div>Welcome Guest</div>
-                </div>
-            )
-        }
+        // Method 3:
+        return(
+            this.state.isLoggedIn ?
+            <div>Welcome Tom Brady</div> :
+            <div>Welcome Peyton Manning Guest</div>
+        )
+
+        // Method 2:        
+        // let message
+        // if (this.state.isLoggedIn) {
+        //     message = <div>Welcome Peter</div>
+        // } else {
+        //     message = <div>Welcome Guest</div>
+        // }
+        // return <div>{message}</div>
+
+        // Method 1:
+        // if (this.state.isLoggedIn) {
+        //     return (
+        //         <div>
+        //             <div>Welcome SvMathTutor</div>
+        //         </div>
+        //     )
+        // } else {
+        //     return (
+        //         <div>
+        //             <div>Welcome Guest</div>
+        //         </div>
+        //     )
+        // }
+
     }
 }
 
