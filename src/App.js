dd5a25aa29a3e30df6631f81d859435cd321b8f7
rob4abcba <rob_abcba@yahoo.com>
2019-07-06 20:20:49 -0700

    Add styling CSS in several different ways.

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 class App extends React.Component {
   constructor(props) {
@@ -39,6 +43,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
+        <Inline />
+        <Stylesheet primary={true} />
         <NameList />
         <UserGreeting />
         <ParentComponent />
