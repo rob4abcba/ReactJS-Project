5f948d2af8768322c3ccad886d859bf8bf36f73a
rob4abcba <rob_abcba@yahoo.com>
2019-07-09 11:18:15 -0700

    Add Component Updating Lifecycle Methods

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -20,13 +20,32 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount')
     }
 
+    shouldComponentUpdate() {
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'svmathtutor'
+        })
+    }
+
     render() {
         console.log('LifecycleA render')
         return (
             <div>
-                <div>
-                    LifecycleA
-            </div>
+                <div>LifecycleA</div>
+                <button onClick={this.changeState}>Change State</button>
                 <LifecycleB />
             </div>
         )
