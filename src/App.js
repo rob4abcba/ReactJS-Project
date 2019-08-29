bad192c0af5507359e0e9bafb90d4d4a2d04ed94
rob4abcba <rob_abcba@yahoo.com>
2019-08-29 13:41:48 -0700

    "Ctrl a" then "Shift Alt f" to prettify format App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import Todos1 from './components/Todos1';
-import './App.css';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import MyGreet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline'
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form'
-import LifecycleA from './components/LifecycleA'
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput'
-import FRParentInput from './components/FRParentInput'
-import PortalDemo from './components/PortalDemo';
-import Todos2 from './Todos2';
-import AddTodo2 from './AddTodo2'
+import React from "react";
+import Todos1 from "./components/Todos1";
+import "./App.css";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import MyGreet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
+import UserGreeting from "./components/UserGreeting";
+import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
+import Form from "./components/Form";
+import LifecycleA from "./components/LifecycleA";
+import FragmentDemo from "./components/FragmentDemo";
+import Table from "./components/Table";
+import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
+import FRParentInput from "./components/FRParentInput";
+import PortalDemo from "./components/PortalDemo";
+import Todos2 from "./Todos2";
+import AddTodo2 from "./AddTodo2";
 
 class App extends React.Component {
   // constructor(props) {
@@ -53,33 +53,30 @@ class App extends React.Component {
   // }
 
   state = {
-    todos2: [
-      {id: 1, content: 'Buy milk'},
-      {id: 2, content: 'Play Mario'}
-    ]
-  }
-  deleteTodo = (id) => {
+    todos2: [{ id: 1, content: "Buy milk" }, { id: 2, content: "Play Mario" }]
+  };
+  deleteTodo = id => {
     console.log(id);
     const todos2 = this.state.todos2.filter(todo2 => {
-      return todo2.id !== id
-    })
+      return todo2.id !== id;
+    });
     this.setState({
       todos2: todos2
-    })
-  }
-  addTodo2 = (todo2) => {
+    });
+  };
+  addTodo2 = todo2 => {
     todo2.id = Math.random();
     let todos2 = [...this.state.todos2, todo2];
     this.setState({
       todos2: todos2
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos2 todos2={this.state.todos2} deleteTodo={this.deleteTodo}/>
+        <Todos2 todos2={this.state.todos2} deleteTodo={this.deleteTodo} />
         <AddTodo2 addTodo2={this.addTodo2} />
         <PortalDemo />
         <FRParentInput />
@@ -90,7 +87,7 @@ class App extends React.Component {
         <FragmentDemo />
         {/* <LifecycleA /> */}
         <Form />
-        <h1 className='error'>Error</h1>
+        <h1 className="error">Error</h1>
         <h1 className={styles.success}>Success</h1>
         <Inline />
         <Stylesheet primary={true} />
